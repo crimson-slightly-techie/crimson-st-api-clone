6733708a84564f331eee780473cb3b235c7bc40c
Odame <kwesi.danquah@daaseng.com>
2024-03-22 20:47:58 +0000

    Included aplication routes in app entry point

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const morgan = require("morgan")
-const db = require("./config/db")
-const cookieParser = require("cookie-parser")
-require("dotenv").config()
+const express = require("express");
+const app = express();
+const router = require("./routes");
+const cors = require("cors");
+const morgan = require("morgan");
+const db = require("./config/db");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 // mongodb connection
-db()
+db();
 
 // Middleware Connections
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
-app.use(express.json({ extended: true }))
-app.use(cookieParser())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.json({ extended: true }));
+app.use(cookieParser());
 
 // Routes
 
 app.get("/api/v2", (req, res) => {
-	res.send("hi there")
-})
+  res.send("hi there");
+});
+
+app.use("/api/v2", router);
 // Connection
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`App running in port: http://localhost:${PORT}`)
-})
+  console.log(`App running in port: http://localhost:${PORT}`);
+});
