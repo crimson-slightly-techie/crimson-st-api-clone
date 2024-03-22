c2cf9b8e40d9b4842ac885057de9bd2170c559a9
Odame <kwesi.danquah@daaseng.com>
2024-03-22 20:49:11 +0000

    Defined post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,53 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const PostController = require("../controllers/PostController.js");
+const validatePostCreation = require("../middlewares/StorePostMiddleware.js");
 
-// Create a new router instance.
 const postRoutes = express.Router();
+
+postRoutes.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const posts = await PostController.getAllPosts(req, res);
+    res.send(posts);
+  })
+);
+
+postRoutes.post(
+  "/",
+  validatePostCreation,
+  asyncHandler(async (req, res) => {
+    const post = await PostController.storePost(req, res);
+
+    res.send(post);
+  })
+);
+
+// Route to get a specific post by ID
+postRoutes.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const post = await PostController.getPost(req, res);
+
+    res.send(post);
+  })
+);
+
+// Route to update a post by ID
+postRoutes.put(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const post = await PostController.updatePost(req, res);
+    res.send(post);
+  })
+);
+
+// Route to delete a post by ID
+postRoutes.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    return await PostController.deletePost(req, res);
+  })
+);
 
 module.exports = postRoutes;
