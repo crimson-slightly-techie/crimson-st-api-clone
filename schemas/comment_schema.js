35b09d1767a797b6486ee2dcbefdc1d37c121979
Odame <kwesi.danquah@daaseng.com>
2024-03-22 15:53:16 +0000

    feat(Comment): Defined model and schema

---
+++ schemas/comment_schema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    post_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = commentSchema;
