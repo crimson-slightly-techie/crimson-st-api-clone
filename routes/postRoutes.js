17dd6cf5b7d5784750609519b6cdcc1969bc7904
Odame <kwesi.danquah@daaseng.com>
2024-03-22 16:56:22 +0000

    Initial post route definition

---
+++ routes/postRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+// Create a new router instance.
+const postRoutes = express.Router();
+
+module.exports = postRoutes;
